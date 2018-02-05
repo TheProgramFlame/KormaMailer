14ddd5f554313e0f30f41a29284f214473c7c77d
Daniël Vlam <danielvlam@hotmail.com>
2018-02-05 16:35:49 +0100

    date picker (under construction)

--- KormaMailer/wwwroot/js/Controllers/MailController.js
+++ KormaMailer/wwwroot/js/Controllers/MailController.js
@@ -1,7 +1,20 @@
 ﻿app.controller('MailController', ['$scope', function ($scope) {
-    $scope.templates = [
+
+    var vm = $scope;
+
+    vm.templates = [
         "template 1",
         "template 2",
         "template 3"
+    ];
+    vm.functions = [
+        "Testfunctie 1",
+        "Testfunctie 2",
+        "Testfunctie 3"
+    ];
+    vm.groupes = [
+        "Testgroep 1",
+        "Testgroep 2",
+        "Testgroep 3"
     ];
 }]);
