c746a15bc2a31e9bcd096c5278495a06e7f25e24
Berend Hulshof <berend-hulshof@hotmail.com>
2018-02-05 16:35:05 +0100

    Mail settings en template uitlees functie toegevoegd

--- KormaMailer/wwwroot/js/Controllers/MailController.js
+++ KormaMailer/wwwroot/js/Controllers/MailController.js
@@ -1,7 +1,60 @@
 ﻿app.controller('MailController', ['$scope', function ($scope) {
-    $scope.templates = [
-        "template 1",
-        "template 2",
-        "template 3"
+    var vm = $scope;
+
+    vm.templates = [
+        "template1",
+        "template2",
+        "template3"
     ];
+
+    vm.mailsettings = {
+        template: null,
+        datumSend: null,
+        onderwerp: null,
+        verzender: null,
+        antwoord: null,
+        contents: {
+            files: null,
+            fileLinks: null,
+            message: null
+        },
+        receiverSettings: {
+            funcGroup: null,
+            sysGroup: null,
+            subDateGroup: null
+        }
+    };
+
+    vm.LoadTemplate = function () {
+        if (vm.mailsettings.template != null) {
+            var url = 'Templates/Mail/' + vm.mailsettings.template + '.html';
+            readTemplate(url);
+            var messageContent = localStorage.receivedTemplate;
+
+            if (messageContent != "") {
+                vm.mailsettings.contents.message = messageContent;
+
+                CKEDITOR.instances.editor.setData(messageContent, function () {
+                    this.checkDirty(true);
+                });
+            } else {
+                alert("Template niet gevonden!");
+            }
+        }
+    };
+
+
+    function readTemplate(url) {
+        var request = new XMLHttpRequest();
+        request.open("get", url, false);
+        request.onreadystatechange = function () {
+            if (request.readyState === 4) {
+                localStorage.setItem('receivedTemplate', request.responseText);
+            }
+            if (request.status == 404) {
+                localStorage.setItem('receivedTemplate', "");
+            }
+        }
+        request.send(null);
+    }
 }]);
